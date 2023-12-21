6192405f5e0b991a3dd86ba8c0c4dbf4730101d9
Mifaresss <9803211@gmail.com>
2023-12-21 20:12:14 +0200

    Up hero img quality

--- src/modules/HeroSection/index.tsx
+++ src/modules/HeroSection/index.tsx
@@ -14,6 +14,7 @@ export function HeroSection({}: Props) {
 			<Image
 				className={getClsNames(s.backgroundImg)}
 				src={mainBackgroundImg}
+				quality={100}
 				alt='Ukraine landscape'
 			/>
 			<div className={getClsNames(s.content)}>
